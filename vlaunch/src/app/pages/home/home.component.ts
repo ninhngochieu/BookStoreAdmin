edbd6a68ea7c273716ddd7ee01377edba9a89906
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2021-05-17 23:08:40 +0700

    Yêu cầu fix layout

--- vlaunch/src/app/pages/home/home.component.ts
+++ vlaunch/src/app/pages/home/home.component.ts
@@ -3,6 +3,9 @@ import { UserProfile } from 'src/app/models/user_profile';
 import { AuthService } from 'src/app/modules/auth/auth.service';
 import {OrderService} from '../order/order.service';
 import {Invoice} from '../order/order.component';
+import {ProductService} from "../products/products.service";
+import {ProductModifyService} from "../product-modify/product-modify.service";
+import {BookProfile} from "../../models/book";
 
 @Component({
   selector: 'app-home',
@@ -11,7 +14,8 @@ import {Invoice} from '../order/order.component';
 })
 export class HomeComponent implements OnInit {
   private invoiceList: Invoice[] = [];
-  constructor(public authService: AuthService, private orderService: OrderService) {}
+  private bookList: BookProfile[] = [];
+  constructor(public authService: AuthService, private orderService: OrderService, private productService: ProductModifyService) {}
 
   profile: UserProfile;
 
@@ -22,6 +26,11 @@ export class HomeComponent implements OnInit {
     this.orderService.getAllInvoice();
     this.orderService.$invoice.subscribe(res => {
       this.invoiceList = res;
+    });
+    this.productService.getAllBook();
+    this.productService.book.subscribe(res => {
+      this.bookList = res;
+      console.log(res);
     });
   }
 
@@ -58,6 +67,47 @@ export class HomeComponent implements OnInit {
     this.invoiceList.forEach(x => {
       if (x.statusId === 3){
         sum += 1;
+      }
+    });
+    return sum;
+  }
+
+  getTotalMoney(): any {
+    let sum = 0;
+    this.invoiceList.forEach(x => {
+      if (x.statusId === 2){
+        sum += x.totalMoney;
+      }
+    });
+    return sum;
+  }
+
+  getActiveProduct(): any {
+    let sum = 0;
+    this.bookList.forEach(x => {
+      if (!x.private){
+        sum += 1;
+      }
+    });
+    return sum;
+  }
+
+  getOutstockProduct(): any {
+    let sum = 0;
+    this.bookList.forEach(x => {
+      if (x.quantity === 0){
+        sum += 1;
+      }
+    });
+    return sum;
+  }
+
+  getPrivateProduct(): any {
+    let sum = 0;
+    this.bookList.forEach(x => {
+      if (x.private){
+        sum += 1;
+        console.log(x)
       }
     });
     return sum;
